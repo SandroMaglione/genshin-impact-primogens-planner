a111d670ccfa3b79a9e91147ea9595d369fbe408
SandroMaglione <lass.maglio@gmail.com>
2025-03-11 05:18:29 +0100

    add link to Artifact

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -51,6 +51,15 @@ function RootComponent() {
           </li>
           <li>
             <a
+              href="https://www.crimsonwitch.com/"
+              target="_blank"
+              className="text-sm font-light hover:underline"
+            >
+              Artifact
+            </a>
+          </li>
+          <li>
+            <a
               href="https://kamigame.jp/genshin/index.html#%E5%8E%9F%E7%A5%9E%E3%81%AE%E3%82%AD%E3%83%A3%E3%83%A9--%E5%85%A8%E3%82%AD%E3%83%A3%E3%83%A9%E4%B8%80%E8%A6%A7"
               target="_blank"
               className="text-sm font-light hover:underline"
