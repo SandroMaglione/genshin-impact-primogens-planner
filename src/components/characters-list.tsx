a1078462513a72744ed2f19d77d070b68f4393de
SandroMaglione <lass.maglio@gmail.com>
2025-03-17 07:03:57 +0100

    characters full image

--- src/components/characters-list.tsx
+++ src/components/characters-list.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import { useState } from "react";
-import { characters } from "../lib/assets/characters";
+import fullCharacters from "../assets/characters.json";
 import { useActionEffect } from "../lib/hooks/use-action-effect";
 import { useCharacter } from "../lib/hooks/use-character";
 import { Dexie } from "../lib/services/dexie";
@@ -19,20 +19,22 @@ export default function CharactersList() {
           onChange={(e) => setSearch(e.target.value)}
           className="border border-grey rounded-md bg-white px-3 py-2 font-light text-sm flex-1"
         />
-        <p className="font-light text-sm text-right">{`${data?.length ?? 0} characters`}</p>
+        <p className="font-light text-sm text-right">{`${data?.length ?? 0}/${fullCharacters.length} characters`}</p>
       </div>
-      <div className="grid grid-cols-8 gap-2">
-        {Object.entries(characters)
-          .filter(([name]) => name.toLowerCase().includes(search.toLowerCase()))
-          .map(([name, src]) => (
+      <div className="grid grid-cols-4 gap-2">
+        {fullCharacters
+          .filter(({ english_name }) =>
+            english_name.toLowerCase().includes(search.toLowerCase())
+          )
+          .map(({ id, name }) => (
             <button
-              key={name}
+              key={id}
               type="button"
               onClick={() => onToggle({ name })}
               className="group hover:cursor-pointer"
             >
               <img
-                src={src}
+                src={`/images/${id}.png`}
                 alt={name}
                 title={name}
                 className={clsx(
